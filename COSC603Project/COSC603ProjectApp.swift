d8a4f94ed27c0b123ff45118e44888fb8a4a42ae
Juan Pablo Combariza <jp.combariza833@gmail.com>
2021-04-27 12:59:23 -0400

    Initial Commit

---
+++ COSC603Project/COSC603ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  COSC603ProjectApp.swift
+//  COSC603Project
+//
+//  Created by JUAN PABLO COMBARIZA MEJIA on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct COSC603ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COSC603Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COSC603Project
+//
+//  Created by JUAN PABLO COMBARIZA MEJIA on 4/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
